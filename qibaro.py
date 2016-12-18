2a96a4d905cf84ce8492f1dde1f94d140a8d6870
beckojr <camara.becko@gmail.com>
2016-12-18 13:35:48 +0100

    Mise en forme finale du formulaire d'inscription, layout des pages principales fini et début de la page principale

--- qibaro.py
+++ qibaro.py
@@ -15,6 +15,10 @@ def inscription():
 def page_layout():
     return render_template('/content/page-layout.html')
 
+@app.route('/home/')
+def home():
+    return render_template('/content/home.html')
+
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port='5000')
